245a32be5bf0ec7e5d7383e29548cf51d1b34d0b
danygk <danyboy.rules@gmail.com>
2024-10-30 19:46:14 -0600

    Create x.bat

---
+++ x.bat
@@ -0,0 +1,27 @@
+@echo off
+
+
+msg * i got your wifi
+ 
+powershell (get-netconnectionProfile).Name
+color 2
+
+powershell Start-Sleep -m 3000
+
+netsh wlan show profile
+
+powershell Start-Sleep -m 3000
+msg * you suck with security
+
+msg * who knows who did this to you
+ msg * its your fault by opening this 
+
+msg * why dont you check viruses
+msg * stop being dumb
+
+msg * i got all of your files also
+
+msg * HAHAHAHA
+msg * i have the power to delet important files
+powershell Start-Sleep -m 1500
+msg * take it as a lesson to never open random files alright?
