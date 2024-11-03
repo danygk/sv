8f5e62e97eadba0c005b3aab09d5903aa5f5bfc4
danygk <160291951+danygk@users.noreply.github.com>
2024-11-03 16:44:26 -0600

    Add files via upload

---
+++ code.bat
@@ -0,0 +1,214 @@
+@echo off
+powershell Start-Sleep -m 1500
+msg * you suck with security
+
+msg * who knows who did this to you
+ msg * its your fault by opening this 
+
+msg * why dont you check viruses
+msg * stop being dumb
+msg * im gonna cause chaos in any second :)
+powershell Start-Sleep -m 15000
+msg * get rickrolled
+
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+powershell Start-Sleep -m 100
+
+
+
+msg * goodluck hahaha
+del "C:\Users\danie\OneDrive\Escritorio\nw.exe
+s
+y
